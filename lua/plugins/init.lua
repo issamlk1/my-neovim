a2bbff81e067d65a175f942107dd65a752509736
issam <admin@mlk1.com>
2025-05-06 10:13:01 +0300

    my-neo-vim-finally-worked

---
+++ lua/plugins/init.lua
@@ -0,0 +1,117 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+
+
+ {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    ft = "rust",
+    config = function ()
+      local mason_registry = require('mason-registry')
+      local codelldb = mason_registry.get_package("codelldb")
+      local extension_path = codelldb:get_install_path() .. "/extension/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path.. "lldb/lib/liblldb.so"
+	-- If you are on Linux, replace the line above with the line below:
+	-- local liblldb_path = extension_path .. "lldb/lib/liblldb.so" instead of "dylib"
+      local cfg = require('rustaceanvim.config')
+
+      vim.g.rustaceanvim = {
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+      }
+    end
+  },
+
+  {
+    'rust-lang/rust.vim',
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  {
+    'mfussenegger/nvim-dap',
+    config = function()
+			local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+		end,
+  },
+
+  {
+    'rcarriga/nvim-dap-ui', 
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+			require("dapui").setup()
+		end,
+  },
+
+  {
+    'saecki/crates.nvim',
+    tag = 'stable',
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          crates = {
+            enabled = true,
+            max_results = 8,
+            min_chars = 3
+          },
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
+  },
+{
+    "github/copilot.vim",
+    lazy = false,
+    config = function() -- Mapping tab is already used in NvChad
+      vim.g.copilot_no_tab_map = true; -- Disable tab mapping
+      vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
+    end
+  }
+
+
+  -- {
+  -- 	"nvim-reesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
